c8fd0843f0af3603cf259bdb2a9b9d7f7fc5015b
rajbardhan568 <rajbardhan568@gmail.com>
2026-05-22 02:56:47 +0530

    fix

--- client/src/components/TeacherDash.js
+++ client/src/components/TeacherDash.js
@@ -249,7 +249,7 @@ const TeacherDash = ({ teacherId }) => {
           .text("ATTENDANCE REPORT", 14, 22);
 
         doc.setFontSize(11).setTextColor(80).setFont("helvetica", "normal");
-        doc.text(`Faculty: ${User.name || "Faculty"}`, 14, 35);
+        doc.text(`Faculty: ${user.name || "Faculty"}`, 14, 35);
         doc.text(`Subject: ${subject.subjectName} | Sem: ${subject.semester}`,14,42,);
         doc.text(`Total Classes: ${subject.totalClasses}`, 14, 49);
 
