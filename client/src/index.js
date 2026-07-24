fc36453949e0e4a2bc238c293582450c8c78b2cd
rajbardhan568 <rajbardhan568@gmail.com>
2026-07-25 00:24:15 +0530

    fix

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+serviceWorkerRegistration.register();
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +13,11 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .catch(err => console.log(err));
+  });
+}
